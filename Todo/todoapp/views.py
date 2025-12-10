95c7ad07b08ac8a3edd076fa712512c4782812de
Chanchal Dhote <your-email@example.com>
2025-12-11 00:08:48 +0530

    added

---
+++ Todo/todoapp/views.py
@@ -0,0 +1,19 @@
+# from django.shortcuts import render,redirect,get_object_or_404
+# from django.http import HttpResponse
+# from .models import Task
+
+# # Create your views here.
+# def addTask(request):
+#      task=request.POST['task']
+#      Task.objects.create(task=task)
+#     #  print(request.POST)
+#      return redirect('home')
+
+
+
+# def mark_as_done(request,pk):
+#      task=get_object_or_404(Task,pk=pk)
+#      #task=Task.objects.get(id=pk)
+#      task.is_completed=True
+#      task.save()
+#      return redirect('home')
